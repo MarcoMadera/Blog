81541e9a090cfcd6d2ca20ce9c5abc8147ed2c78
MarcoMadera <marcoseguro2@gmail.com>
2020-08-20 05:31:00 -0500

    rss update

--- site.config.js
+++ site.config.js
@@ -10,6 +10,7 @@ module.exports = {
       "Esta es mi página web personal en la que estaré compartiendo cosas interesantes sobre la web",
     siteUrl: "https://marcomadera.com/",
     language: "es-MX",
+    logo: "https://marcomadera.com/logo512.png",
     social: {
       twitter: "madera_marco",
       gitHub: "MarcoMadera",
