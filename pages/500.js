bcd9c9a7368efe302894283cb67b00e585d86cb9
MarcoMadera <me@marcomadera.com>
2021-03-30 23:18:20 -0600

    added 500 error page

---
+++ pages/500.js
@@ -0,0 +1,24 @@
+import Seo from "../components/Seo";
+
+export default function Custom404() {
+  return (
+    <main id="main">
+      <Seo title="😫 500 - Servidor? | Marco Madera" />
+      <h1>500?</h1>
+      <p>Ha ocurrido un error en el servidor</p>
+      <style jsx>{`
+        h1 {
+          margin: 0;
+          font-size: 100px;
+        }
+        main {
+          height: calc(100vh - 257px);
+          text-align: center;
+        }
+        p {
+          font-size: 30px;
+        }
+      `}</style>
+    </main>
+  );
+}
