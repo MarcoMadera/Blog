dfbbb01a55265b7e98b52619abfcb02c498f92ce
MarcoMadera <me@marcomadera.com>
2022-01-22 20:12:00 -0600

    remove preact causing some bugs

--- next.config.mjs
+++ next.config.mjs
@@ -19,19 +19,12 @@ const nextConfig = {
       },
     ];
   },
-  webpack: (config, { isServer, dev }) => {
+  webpack: (config) => {
     const { pathname: domhandlerPath } = new URL(
       "node_modules/domhandler",
       import.meta.url
     );
     config.resolve.alias["domhandler"] = domhandlerPath;
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
     return config;
   },
   async redirects() {
