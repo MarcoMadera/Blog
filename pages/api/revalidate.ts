b801abe844fb9f1c22e933f8f19fdb123c2afc3b
Marco Madera <marcoseguro2@gmail.com>
2022-10-19 14:07:44 -0700

    add revalidate api (#34)

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function revalidate(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  if (req.query.secret !== process.env.SECRET_KEY) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  try {
+    await res.revalidate("/");
+    return res.json({ revalidated: true });
+  } catch (err) {
+    return res.status(500).send("Error revalidating");
+  }
+}
