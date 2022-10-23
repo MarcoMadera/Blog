e9b0222fbb4da2d0e35703379a910b016ab16033
MarcoMadera <marcoseguro2@gmail.com>
2022-10-23 03:20:18 -0700

    fix folder name

--- components/MicroMemories/index.tsx
+++ components/MicroMemories/index.tsx
@@ -4,7 +4,7 @@ import { HomeData } from "types/posts";
 import { database } from "lib/firebase/client";
 import { limitToLast, onValue, query, ref } from "firebase/database";
 import { IMicroMemory } from "types/microMemories";
-import MicroMemory from "components/MicroMemory";
+import MicroMemory from "components/MicroMemories/MicroMemory";
 import { ReactElement } from "rehype-react/lib";
 import ActionButton from "components/ActionButton";
 
