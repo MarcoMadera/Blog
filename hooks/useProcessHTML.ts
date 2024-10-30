ac5d5dd8a691bb0d328ab6953d70a2cd2ec73597
MarcoMadera <marcoseguro2@gmail.com>
2024-10-30 12:46:57 -0700

    fix tweet layout shift

--- hooks/useProcessHTML.ts
+++ hooks/useProcessHTML.ts
@@ -1,10 +1,4 @@
-import {
-  Children,
-  ReactNode,
-  isValidElement,
-  useEffect,
-  useState,
-} from "react";
+import { Children, ReactNode, isValidElement } from "react";
 import * as prod from "react/jsx-runtime";
 import rehypeParse from "rehype-parse";
 import rehypeReact from "rehype-react";
@@ -14,25 +8,16 @@ import { unified } from "unified";
 const production = { Fragment: prod.Fragment, jsx: prod.jsx, jsxs: prod.jsxs };
 
 export function useProcessHTML(text?: string): ReactNode[] | null {
-  const [content, setContent] = useState<ReactNode[] | null>(null);
+  if (!text) return null;
 
-  useEffect(
-    function () {
-      (async function () {
-        if (!text) return;
-        const file = await unified()
-          .use(rehypeParse, { fragment: true })
-          .use(rehypeReact, production)
-          .process(text);
-        const children = Children.map(file.result, (child) => {
-          if (isValidElement(child)) return child;
-          return null;
-        });
-        setContent(children);
-      })();
-    },
-    [text]
-  );
+  const file = unified()
+    .use(rehypeParse, { fragment: true })
+    .use(rehypeReact, production)
+    .processSync(text);
+  const children = Children.map(file.result, (child) => {
+    if (isValidElement(child)) return child;
+    return null;
+  });
 
-  return content;
+  return children;
 }
